fix sign to text 1D-cnn v0.3

KCH/signtotext/npy_checker.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import time
 
 # 1️⃣ 파일 경로 입력
-npy_file = 'output_npy/VXPAKOKS240779250_너나같다1_C.npy'  # ← 본인 npy 경로로 수정
+npy_file = 'output_npy/VXPAKOKS241094050_회사1_C.npy'  # ← 본인 npy 경로로 수정
 
 # 2️⃣ 데이터 로드
 data = np.load(npy_file)