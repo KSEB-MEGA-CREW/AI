b3d08245c9ee5e988b66f1183fe2f35bd66a823c
Hyuk <dh95229505@gmail.com>
2025-07-31 15:10:22 +0900

    feat: Separate inferential code train_model.py(modefide) inference.py, add.py(add) The inference module, named inference.py has been developed as a distinct component from the train_model.pu code. To subsequently employ in for the mapping of data to JSON files

--- text_to_word/config.py
+++ text_to_word/config.py
@@ -4,10 +4,10 @@
 MODEL_NAME = "ehekaanldk/kobart2ksl-translation"
 
 # 윈도우
-# DATA_PATH = "C:/Users/Hyuk/Documents/대학/부트켐프/메인 프로젝트/AI/text_to_word/preprocessed_data/processed_data.jsonl"
-# OUTPUT_DIR = "C:/Users/Hyuk/Documents/대학/부트켐프/메인 프로젝트/AI/text_to_word/kobart-finetuned-ksl-glosser"
-# CUSTOM_TOKENIZER = "C:/Users/Hyuk/Documents/대학/부트켐프/메인 프로젝트/AI/text_to_word/kobart-custom-tokenizer"
-# TOKENIZER_SAVE_DIR = "C:/Users/Hyuk/Documents/대학/부트켐프/메인 프로젝트/AI/text_to_word/ksl-tokenizer"
+DATA_PATH = "C:/Users/Hyuk/Documents/대학/부트켐프/메인 프로젝트/AI/text_to_word/preprocessed_data/processed_data.jsonl"
+OUTPUT_DIR = "C:/Users/Hyuk/Documents/대학/부트켐프/메인 프로젝트/AI/text_to_word/kobart-finetuned-ksl-glosser"
+CUSTOM_TOKENIZER = "C:/Users/Hyuk/Documents/대학/부트켐프/메인 프로젝트/AI/text_to_word/kobart-custom-tokenizer"
+TOKENIZER_SAVE_DIR = "C:/Users/Hyuk/Documents/대학/부트켐프/메인 프로젝트/AI/text_to_word/ksl-tokenizer"
 
 # 리눅스 1
 # DATA_PATH = "/home/kdh/KSEB/0723/text_to_word/preprocessed_data/processed_data.jsonl"
@@ -16,7 +16,7 @@ MODEL_NAME = "ehekaanldk/kobart2ksl-translation"
 # TOKENIZER_SAVE_DIR = "/home/kdh/KSEB/0723/text_to_word/ksl-tokenizer"
 
 #리눅스 2
-DATA_PATH = "/home/202044005/KSEB/text_to_word/preprocessed_data/processed_data.jsonl"
-OUTPUT_DIR = "/home/202044005/KSEB/text_to_word/kobart-finetuned-ksl-glosser"
-CUSTOM_TOKENIZER = "/home/202044005/KSEB/text_to_word/kobart-custom-tokenizer"
-TOKENIZER_SAVE_DIR = "/home/202044005/KSEB/text_to_word/ksl-tokenizer"
+# DATA_PATH = "/home/202044005/KSEB/text_to_word/preprocessed_data/processed_data.jsonl"
+# OUTPUT_DIR = "/home/202044005/KSEB/text_to_word/kobart-finetuned-ksl-glosser"
+# CUSTOM_TOKENIZER = "/home/202044005/KSEB/text_to_word/kobart-custom-tokenizer"
+# TOKENIZER_SAVE_DIR = "/home/202044005/KSEB/text_to_word/ksl-tokenizer"
---
+++ text_to_word/mapping_point.py
@@ -0,0 +1,4 @@
+import os
+
+def map_gloss_to_point(gloss_list:list):
+    pass
